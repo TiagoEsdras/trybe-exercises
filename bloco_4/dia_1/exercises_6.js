68cb216dde64c240a52aec94141a387ac370b048
TIAGO <tiagoesdrasvieira@gmail.com>
2020-08-27 17:37:28 -0300

    Exercicio 6 - Maior de dois numeros

---
+++ bloco_4/dia_1/exercises_6.js
@@ -0,0 +1,10 @@
+//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+let a = 27, b = 34;
+
+if (a > b) {
+    console.log(a);
+}
+else {
+    console.log(b);
+}
