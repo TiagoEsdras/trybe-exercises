5f66d43e66f4b643e602430bcbe6986ab05816ae
TIAGO <tiagoesdrasvieira@gmail.com>
2020-08-28 17:43:47 -0300

    Exercicio 2 - Percorrendo e somando array

--- bloco_4/dia_2/arry.js
+++ bloco_4/dia_2/arry.js
@@ -2,8 +2,19 @@
 
 Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();*/
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let i = 0; i < numbers.length; i += 1) {
     console.log(numbers[i]);    
 }
+*/
+
+//Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0
+
+for (let i = 0; i < numbers.length; i += 1) {
+    sum += numbers[i];     
+}
+console.log(sum);
