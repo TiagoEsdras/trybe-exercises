3e478511913d3371c6f951723d1f80c55542356a
TIAGO <tiagoesdrasvieira@gmail.com>
2020-08-28 17:48:22 -0300

    Exercicio 3 - Calculando media aritmetica de array

--- bloco_4/dia_2/arry.js
+++ bloco_4/dia_2/arry.js
@@ -11,10 +11,23 @@ for (let i = 0; i < numbers.length; i += 1) {
 
 //Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0
 
 for (let i = 0; i < numbers.length; i += 1) {
     sum += numbers[i];     
 }
-console.log(sum);
+console.log(sum);*/
+
+/*Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let count = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+    sum += numbers[i];
+    count += 1;     
+}
+console.log(sum / count);
