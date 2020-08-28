fb723e40ce79283a716217c262b17c5c8ae20534
TIAGO <tiagoesdrasvieira@gmail.com>
2020-08-28 17:52:09 -0300

    Exercicio 4 - Estabelecendo condicao

--- bloco_4/dia_2/arry.js
+++ bloco_4/dia_2/arry.js
@@ -22,6 +22,18 @@ console.log(sum);*/
 /*Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.*/
 
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let count = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+    sum += numbers[i];
+    count += 1;     
+}
+console.log(sum / count);*/
+
+/*Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";*/
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
 let count = 0;
@@ -30,4 +42,9 @@ for (let i = 0; i < numbers.length; i += 1) {
     sum += numbers[i];
     count += 1;     
 }
-console.log(sum / count);
+if ((sum / count) <= 20){
+    console.log(`valor menor ou igual a 20`);
+}
+else{
+    console.log(`valor maior que 20`);
+}
