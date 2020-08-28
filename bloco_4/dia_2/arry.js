5e0110246edc829de6e61737aa063057134f7f79
TIAGO <tiagoesdrasvieira@gmail.com>
2020-08-28 17:56:26 -0300

    Exercicio 5 - Descobrindo maior numero de um array

--- bloco_4/dia_2/arry.js
+++ bloco_4/dia_2/arry.js
@@ -34,7 +34,7 @@ console.log(sum / count);*/
 
 /*Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";*/
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
 let count = 0;
 
@@ -47,4 +47,15 @@ if ((sum / count) <= 20){
 }
 else{
     console.log(`valor maior que 20`);
+}*/
+
+//Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let higherNumber = 0;
+for (let i = 0; i < numbers.length; i += 1) {
+     if (numbers[i] > higherNumber){
+         higherNumber = numbers[i];
+     }  
 }
+console.log(higherNumber);
