27df3856e1d295716cf52c7fd7c4b5f0254ae67d
TIAGO <tiagoesdrasvieira@gmail.com>
2020-08-31 23:11:58 -0300

    Bonus 6 - Piramede de asteriscos com meio vazio

--- bloco_4/dia_3/exercises_4.js
+++ bloco_4/dia_3/exercises_4.js
@@ -11,11 +11,11 @@ n = 5
 let n = 5;
 let outPut = '';
 
-let midlleM = (n + 1) / 2;
-let l = midlleM;
-let r = midlleM;
+let middleM = (n + 1) / 2;
+let l = middleM;
+let r = middleM;
 
-for (let i = 0; i <= midlleM; i += 1) {
+for (let i = 0; i <= middleM; i += 1) {
   for (let j = 0; j <= n; j += 1) {
     if (j > r && j < l) {
       outPut += "*";
