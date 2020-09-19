6a8c6b191e06bd829791967c0cdbab2e1a1621ca
TIAGO <tiagoesdrasvieira@gmail.com>
2020-09-19 11:51:27 -0300

    Estilizando formulario

--- bloco_6/dia_1/script.js
+++ bloco_6/dia_1/script.js
@@ -9,3 +9,17 @@ for (let i = 0; i < arrayEstados.length; i += 1) {
     estado.appendChild(option)
 }
 
+let submitBtn = document.querySelector('#button');
+    submitBtn.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    let dataValue = document.querySelector('#data').value;
+    let dataString = dataValue.split('/');
+    if (parseInt(dataString[0]) < 1 || parseInt(dataString[0]) > 31) {
+        alert('O valor do dia deve ser entre 01 e 31');
+    } else if (parseInt(dataString[1]) < 1 || parseInt(dataString[1]) > 12) {
+        alert('O valor de mês deve ser entre 01 e 12');
+    } else if (parseInt(dataString[2]) < 0) {
+        alert('O valor do ano nao pode ser negativo')
+    }
+})
+
