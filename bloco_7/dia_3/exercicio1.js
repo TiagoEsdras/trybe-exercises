5d69d555b08415072222a5a51a3fbed7623a1547
TIAGO <tiagoesdrasvieira@gmail.com>
2020-09-29 20:45:32 -0300

    Exercicio 1

---
+++ bloco_7/dia_3/exercicio1.js
@@ -0,0 +1,13 @@
+// 1- A função sum(a, b) retorna a soma do parâmetro a com o b
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+assert.strictEqual(sum(4, 5), 9);
+assert.strictEqual(sum(0, 0), 0);
+assert.strictEqual(sum(4, '5'), 9);
