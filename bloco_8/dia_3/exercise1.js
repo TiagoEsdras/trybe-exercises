499652286c7da8ed06ec916368d13a69e97691b5
TIAGO <tiagoesdrasvieira@gmail.com>
2020-10-07 19:08:35 -0300

    Exercicio 1

---
+++ bloco_8/dia_3/exercise1.js
@@ -0,0 +1,21 @@
+//Dada uma matriz de matrizes, transforme em uma única matriz.
+const assert = require('assert');
+
+const arrays = [
+    ["1", "2", "3"],
+    [true],
+    [4, 5, 6]
+];
+
+
+function flatten() {
+  // escreva seu código aqui
+  return arrays.reduce((acc, currentValue) => {
+    currentValue.map((array) => {
+      acc.push(array);
+    })
+    return acc;
+  }, [])
+}
+
+assert.deepStrictEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
