5aa991934e14a01cac378021b8bcc1c4ba304860
TIAGO <tiagoesdrasvieira@gmail.com>
2020-10-28 19:10:52 -0300

    Exercicio 2 - c

--- bloco_10/dia_1/myRemove.test.js
+++ bloco_10/dia_1/myRemove.test.js
@@ -7,4 +7,7 @@ describe('myRemove', () => {
   test('Call myRemove([1, 2, 3, 4], 3) return is diferent than [1, 2, 3, 4]', () => {
     expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
   });
+  test('Call myRemove([1, 2, 3, 4], 5) return must be [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
 });
