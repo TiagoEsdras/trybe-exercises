7492a10e511434aa7624872cab7eeb1428fa8de7
TIAGO <tiagoesdrasvieira@gmail.com>
2020-10-28 18:45:58 -0300

    Exercicio 1 - c

--- bloco_10/dia_1/ sum.test.js
+++ bloco_10/dia_1/ sum.test.js
@@ -1,9 +1,16 @@
-const sum = require('./sum.js')
+const sum = require('./sum.js');
+describe('sum', () => {
+  test('sum of 4 and 5 must be 9', () => {
+    expect(sum(4, 5)).toBe(9);
+  });
 
-test('sum of 4 and 5 must be 9', () => {
-  expect(sum(4, 5)).toBe(9);
-});
+  test('sum of 0 and 0 must be 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
 
-test('sum of 0 and 0 must be 0', () => {
-  expect(sum(0, 0)).toBe(0);
+  test('throws an error when a string is passed', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow();
+  });
 });
