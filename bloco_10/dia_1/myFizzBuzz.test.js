737ac2d978c97b77515aa1dc7cab7f2ac1593207
TIAGO <tiagoesdrasvieira@gmail.com>
2020-10-29 10:56:52 -0300

    Exercicio 4 - b

--- bloco_10/dia_1/myFizzBuzz.test.js
+++ bloco_10/dia_1/myFizzBuzz.test.js
@@ -4,4 +4,7 @@ describe('myFizzBuzz', () => {
   test('Calling myFizzBuzz(15) retunr must be fizzbuzz', () => {
     expect(myFizzBuzz(15)).toBe('fizzbuzz');
   });
+  test('Calling myFizzBuzz(9) retunr must be fizz', () => {
+    expect(myFizzBuzz(9)).toBe('fizz');
+  });
 });
