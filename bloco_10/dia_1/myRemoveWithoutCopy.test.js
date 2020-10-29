46628d55e220f5f7dfa72d4336a8681c21039103
TIAGO <tiagoesdrasvieira@gmail.com>
2020-10-29 10:43:14 -0300

    Exercicio 3 - c

--- bloco_10/dia_1/myRemoveWithoutCopy.test.js
+++ bloco_10/dia_1/myRemoveWithoutCopy.test.js
@@ -7,4 +7,9 @@ describe('myRemoveWithCopy', () => {
   test('Calling myRemoveWithoutCopy([1, 2, 3, 4], 3) retunr must be different than [1, 2, 3, 4]', () => {
     expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
   });
+  test('Calling myRemoveWithoutCopy(arr, 1) arr change', () => {
+    const arr = [1, 2, 3, 4];
+    myRemoveWithoutCopy(arr, 1);
+    expect(arr).not.toEqual([arr]);
+  });
 });
