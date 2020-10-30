3f5b77f6dde8a1c4315b8904a7997f46aefdf995
TIAGO <tiagoesdrasvieira@gmail.com>
2020-10-30 03:46:11 -0300

    Exercicio 3

--- bloco_10/dia_2/getUserName.test.js
+++ bloco_10/dia_2/getUserName.test.js
@@ -31,3 +31,20 @@ it('returns an error', () => {
     expect(error).toEqual({ error: 'User with 2 not found.' })
   );*/
 });
+
+it('getUserName(4) return should be "Mark" using async/await', async () => {
+  expect.assertions(1);
+  const response = await getUserName(4);
+  expect(response).toBe('Mark');
+  //return expect(getUserName(4)).resolves.toBe('Mark');
+});
+
+it('returns an error using async/await', async () => {
+  expect.assertions(1);
+  try {
+    await getUserName(2);
+  } catch (error) {
+    expect(error).toEqual({ error: 'User with 2 not found.' });
+  }
+  //return expect(getUserName(2)).rejects.toEqual({ error: 'User with 2 not found.' });
+});
