472d9967632dcadeddade992bf7fca48d5998f95
TIAGO <tiagoesdrasvieira@gmail.com>
2020-12-07 14:49:54 -0300

    Dia 15.1

---
+++ Modulo_2-Front-End/bloco_15/dia_1/testes-react/src/App.test.js
@@ -0,0 +1,23 @@
+// App.test.js
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('Verificando se existe o campo Email.', () => {
+  const { getByLabelText } = render(<App />);
+  const inputEmail = getByLabelText('Email');
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail.type).toBe('email');
+});
+
+test('Verificando se existe um botão', () => {
+  const { getByTestId } = render(<App />);
+  const btn = getByTestId('id-send');
+  expect(btn).toBeInTheDocument();
+});
+
+test('Verificando se existe dois botões', () => {
+  const { getAllByRole } = render(<App />);
+  const buttons = getAllByRole('button');
+  expect(buttons.length).toBe(2);
+});
